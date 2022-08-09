fc48bb9022638e82754333c7c5f39ab596b1131f
Richard Heise <rfhf19@inf.ufpr.br>
2022-08-08 23:29:50 -0300

    locais feitos

--- trabalho-1/RawSocket.h
+++ trabalho-1/RawSocket.h
@@ -10,55 +10,30 @@
 #include <stdio.h>
 
 enum fields {
-    INIT_MARKER = 126
+    INIT_MARKER = 126,
+    OK = 1,
+    NACK = 2,
+    ACK = 3,
+    CD = 6,
+    LS = 7,
+    MKDIR = 8,
+    GET = 9,
+    PUT = 10,
+    ERROR = 17,
+    DESCRIPTOR = 24,
+    DATA_BYTES = 63,
+    LSL = 35,
+    CDL = 36,
+    MKDIRL = 37,
+    END = 46,
+    MAX_DATA_BYTES = 67
 };
 
-typedef __attribute__((__packed__)) struct msg_s {
-    int init_mark : 8;
+typedef struct __attribute__((packed)) msg_s {
+    unsigned int init_mark : 8;
     unsigned int size : 6;
     unsigned int seq : 4;
-    int type : 6;
-    int parity : 8;
+    unsigned int type : 6;
 } msg_t;
 
-int ConexaoRawSocket(char *device) {
-
-    int soquete;
-    struct ifreq ir;
-    struct sockaddr_ll endereco;
-    struct packet_mreq mr;
-
-    soquete = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));  	/*cria socket*/
-    if (soquete == -1) {
-        printf("Erro no Socket\n");
-        exit(-1);
-    }
-
-    memset(&ir, 0, sizeof(struct ifreq));  	/*dispositivo eth0*/
-    memcpy(ir.ifr_name, device, sizeof(device));
-    if (ioctl(soquete, SIOCGIFINDEX, &ir) == -1) {
-        printf("Erro no ioctl\n");
-        exit(-1);
-    }
-        
-
-    memset(&endereco, 0, sizeof(endereco)); 	/*IP do dispositivo*/
-    endereco.sll_family = AF_PACKET;
-    endereco.sll_protocol = htons(ETH_P_ALL);
-    endereco.sll_ifindex = ir.ifr_ifindex;
-    if (bind(soquete, (struct sockaddr *)&endereco, sizeof(endereco)) == -1) {
-        printf("Erro no bind\n");
-        exit(-1);
-    }
-
-
-    memset(&mr, 0, sizeof(mr));          /*Modo Promiscuo*/
-    mr.mr_ifindex = ir.ifr_ifindex;
-    mr.mr_type = PACKET_MR_PROMISC;
-    if (setsockopt(soquete, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mr, sizeof(mr)) == -1)	{
-        printf("Erro ao fazer setsockopt\n");
-        exit(-1);
-    }
-
-    return soquete;
-}
+int ConexaoRawSocket(char *device);
--- trabalho-1/client.c
+++ trabalho-1/client.c
@@ -1,12 +1,147 @@
 #include "RawSocket.h"
+#include <sys/syscall.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h> 
+
+void local_mkdir(unsigned char* dir) {
+    mkdir(dir, 0700);
+}
+
+void local_cd(unsigned char* dir) {
+    chdir(dir);
+}
+
+void local_ls(int opts) {
+    if (opts == 0) {
+        system("ls");
+    } else if (opts == 1) {
+        system("ls -a");
+    } else if (opts == 2) {
+        system("ls -l");
+    } else {
+        system("ls -a -l");
+    }
+}
+
+int get_type(unsigned char* buf, int *opts, unsigned char* dir) {
+
+    int opt_a = 0;
+    int opt_l = 0;
+    unsigned char command[7];
+    unsigned char opt1[63];
+    unsigned char opt2[6];
+
+    sscanf(buf,"%s %s %s", command, opt1, opt2);
+    if(!strncmp(command,"ls", 3)){
+
+        if(!strncmp("-a", opt1, 3) || !strncmp("-a", opt2, 3))
+            opt_a = 1;
+        if(!strncmp("-l", opt1, 3) || !strncmp("-l", opt2, 3))
+            opt_l = 1;
+
+        if(opt_a == 1 && opt_l == 0)
+            *opts = 1;
+        if(opt_a ==0 && opt_l == 1)
+            *opts = 2;
+        if(opt_a == 1 && opt_l == 1)
+            *opts = 3;
+
+        return LSL;
+
+    }
+
+    if(!strncmp("rls",command,3)) {
+
+        if(!strncmp("-a", opt1, 3) || !strncmp("-a", opt2, 3))
+            opt_a = 1;
+        if(!strncmp("-l", opt1, 3) || !strncmp("-l", opt2, 3))
+            opt_l = 1;
+        if(opt_a == 1 && opt_l == 0)
+            *opts = 1;
+        if(opt_a == 0 && opt_l == 1)
+            *opts = 2;
+        if(opt_a == 1 && opt_l == 1)
+            *opts = 3;
+
+        return LS;
+    }
+    if(!strncmp("cd", command, 3)){
+        strncpy(dir, opt1, 63);
+        return CDL;
+    }
+    if(!strncmp("rcd", command, 3)){
+        strncpy(dir, opt1, 63);
+        return CD;
+    }
+    if(!strncmp("mkdir", command, 6)){
+        strncpy(dir, opt1, 63);
+        return MKDIRL;
+    }
+    if(!strncmp("rmkdir", command, 6)){
+        strncpy(dir, opt1, 63);
+        return MKDIR;
+    }
+    else
+        return 0;
+}
+
+void send_msg(unsigned char* buf) {
+    unsigned char* input = calloc(MAX_DATA_BYTES, sizeof(unsigned char));
+    unsigned char* dir = calloc(MAX_DATA_BYTES, sizeof(unsigned char));
+    unsigned char* data = calloc(DATA_BYTES, sizeof(unsigned char));
+    int type, ls_opts;
+    
+    while(1) {
+        char rel_path[MAX_DATA_BYTES];
+        fprintf(stderr,"%s:\n-> ", getcwd(rel_path, sizeof(rel_path)));
+        
+        fgets(input, MAX_DATA_BYTES, stdin);
+
+        ls_opts = 0;
+        type = get_type(input, &ls_opts, dir);
+
+        switch(type) {
+            case LS:
+                //remote_ls();
+                break;
+            case LSL:
+                local_ls(ls_opts);
+                break;
+            case CD:
+                //remote_cd();
+                break;
+            case CDL:
+                local_cd(dir);
+                break;
+            case MKDIR:
+                //remote_mkdir();
+                break;
+            case MKDIRL:
+                local_mkdir(dir);
+                break;
+            default:
+        }
+
+        memset(input, 0, MAX_DATA_BYTES);
+        memset(data, 0, DATA_BYTES);
+    }
+}
 
 int main () {
-    int teste = ConexaoRawSocket("lo");
-    unsigned char batata;
-    while (1) {
-        recvfrom(teste, &batata, 1, 0, NULL, NULL);
-        printf("%c\n", batata);
+
+    int server = ConexaoRawSocket("lo");
+
+    if (server < 0) {
+        perror("Error while creating socket, aborting.\n");
+        exit(-1);
     }
+
+    unsigned char *buffer = calloc(MAX_DATA_BYTES, sizeof(unsigned char));
+
+    system("clear");
+    send_msg(buffer);
+
 
     return 1;
 }
